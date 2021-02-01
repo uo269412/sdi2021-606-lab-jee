e23406c78ce03b5d45a0988420125aafc8981450
uo269412 <uo269412@uniovi.es>
2021-02-01 14:34:49 +0100

    SDI-2021-606-1.8-MVC, Modelo Vista Controlador

--- sdi2021-606-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
+++ sdi2021-606-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
@@ -43,14 +43,9 @@ public class ServletCarrito extends HttpServlet {
 		if (producto != null) {
 			insertarEnCarrito(carrito, producto);
 		}
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Tienda	SDI:	carrito</TITLE></HEAD>");
-		out.println("<BODY>");
-		out.println(carritoEnHTML(carrito) + "<br>");
-		out.println("<a	href=\"index.jsp\">Volver</a></BODY></HTML>");
+		// Retornar la vista con parámetro "carrito"
+		request.setAttribute("paresCarrito", carrito);
+		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request, response);
 	}
 
 	private String carritoEnHTML(HashMap<String, Integer> carrito) {
