d225061fe86d28a38a98507b2e7b539eb0b3b35c
uo269412 <uo269412@uniovi.es>
2021-02-09 17:56:49 +0100

    SDI-2021-606-2.4-Modelo, acceso a datos simple.

---
+++ sdi2021-606-lab-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
@@ -0,0 +1,10 @@
+package com.uniovi.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.uniovi.entities.Mark;
+
+public interface MarksRepository extends CrudRepository<Mark, Long> {
+	
+	
+}
