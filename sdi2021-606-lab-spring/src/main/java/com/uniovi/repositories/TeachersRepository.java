44722b9b08646dace63eab064faa49d20ef2a524
uo269412 <uo269412@uniovi.es>
2021-02-15 14:07:49 +0100

    SDI-606-session2-Ejercicio-complementario2

---
+++ sdi2021-606-lab-spring/src/main/java/com/uniovi/repositories/TeachersRepository.java
@@ -0,0 +1,10 @@
+package com.uniovi.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.uniovi.entities.Teacher;
+
+public interface TeachersRepository extends CrudRepository<Teacher, Long> {
+	
+	
+}
