6372c4bf364ed721b68b9e65dc0ee59ba93bcd37
uo269412 <uo269412@uniovi.es>
2021-03-01 20:39:35 +0100

    SDI-2021-606-5.1-ClasePrincipal.

--- sdi2021-606-lab-spring/src/test/java/com/uniovi/Sdi2021606LabSpringApplicationTests.java
+++ sdi2021-606-lab-spring/src/test/java/com/uniovi/Sdi2021606LabSpringApplicationTests.java
@@ -1,62 +1,13 @@
 package com.uniovi;
 
-import static org.junit.Assert.fail;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
-import org.junit.runners.MethodSorters;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
-
-//Ordenamos las pruebas por el nombre del método
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
 @SpringBootTest
 class Sdi2021606LabSpringApplicationTests {
 
-	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
-	static String Geckdriver024 = "C:\\Program Files\\Mozilla Firefox\\geckodriver024win64.exe";
-	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
-	static String URL = "http://localhost:8090";
-
-	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
-		System.setProperty("webdriver.firefox.bin", PathFirefox);
-		System.setProperty("webdriver.gecko.driver", Geckdriver);
-		WebDriver driver = new FirefoxDriver();
-		return driver;
-	}
-
-	// Antes de cada prueba se navega al URL home de la aplicación
-	@Before
-	public void setUp() {
-		driver.navigate().to(URL);
-	}
-
-	// Después de cada prueba se borran las cookies del navegador
-	@After
-	public void tearDown() {
-		driver.manage().deleteAllCookies();
-	}
-
-	// Antes de la primera prueba
-	@BeforeClass
-	static public void begin() {
-	}
-
-	// Al finalizar la última prueba
-	@AfterClass
-	static public void end() {
-		// Cerramos el navegador al finalizar las pruebas
-		driver.quit();
-	}
-
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	void contextLoads() {
 	}
+
 }
---
+++ sdi2021-606-lab-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -0,0 +1,56 @@
+package com.uniovi.tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+//Ordenamos las pruebas por el nombre del método 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+public class NotaneitorTests {
+
+	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+	static String Geckdriver024 = "C:\\Program Files\\Mozilla Firefox\\geckodriver024win64.exe";
+
+	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
+	static String URL = "http://localhost:8090";
+
+	@Before
+	public void setUp() {
+		driver.navigate().to(URL);
+	}
+
+	@After
+	public void tearDown() {
+		driver.manage().deleteAllCookies();
+	}
+
+	@BeforeClass
+	static public void begin() {
+	}
+
+	@AfterClass
+	static public void end() {
+		driver.quit();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+}
