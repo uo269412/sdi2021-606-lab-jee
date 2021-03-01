41f7128563708d990a3a825f0ab92836dad0f846
uo269412 <uo269412@uniovi.es>
2021-03-01 20:41:51 +0100

    SDI-2021-606-5.3-PO NavView.

--- sdi2021-606-lab-spring/src/test/java/com/uniovi/tests/pageobjects/PO_NavView.java
+++ sdi2021-606-lab-spring/src/test/java/com/uniovi/tests/pageobjects/PO_NavView.java
@@ -17,8 +17,7 @@ public class PO_NavView extends PO_View {
 	 * la página destino.
 	 */
 	public static void clickOption(WebDriver driver, String textOption, String criterio, String textoDestino) {
-		// CLickamos en la opción de registro y esperamos a que se cargue el enlace de
-		// Registro.
+		// CLickamos en la opción de registro y esperamos a que se cargue el enlace de Registro.
 		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "@href", textOption, getTimeout());
 		// Tiene que haber un sólo elemento.
 		assertTrue(elementos.size() == 1);
