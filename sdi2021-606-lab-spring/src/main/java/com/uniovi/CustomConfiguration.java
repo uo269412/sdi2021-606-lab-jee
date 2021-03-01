73e1e1a0cbaa6c291cb681557220e2f79c128b7c
uo269412 <uo269412@uniovi.es>
2021-03-01 14:26:48 +0100

    SDI-2021-606-sesión4-Ejercicio-complementario5

--- sdi2021-606-lab-spring/src/main/java/com/uniovi/CustomConfiguration.java
+++ sdi2021-606-lab-spring/src/main/java/com/uniovi/CustomConfiguration.java
@@ -3,6 +3,7 @@ package com.uniovi;
 import java.util.List;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
@@ -16,6 +17,13 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class CustomConfiguration implements WebMvcConfigurer {
+
+	@Value("${spring.data.web.pageable.page-parameter}")
+	private int page;
+
+	@Value("${spring.data.web.pageable.default-page-size}")
+	private int size;
+
 	@Bean
 	public LocaleResolver localeResolver() {
 		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
@@ -35,10 +43,16 @@ public class CustomConfiguration implements WebMvcConfigurer {
 		registry.addInterceptor(localeChangeInterceptor());
 	}
 
+//	@Override
+//	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+//		int page = 0;
+//		int size = 5;
+//		PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+//		resolver.setFallbackPageable(PageRequest.of(page, size));
+//		argumentResolvers.add(resolver);
+
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-		int page = 0;
-		int size = 5;
 		PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
 		resolver.setFallbackPageable(PageRequest.of(page, size));
 		argumentResolvers.add(resolver);
